4800405cfe6593af4c921df4456f665bcc0a9d0f
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2022-10-31 14:31:42 -0400

    Initial Commit

---
+++ Favourate Things/Favourate_ThingsApp.swift
@@ -0,0 +1,19 @@
+//
+//  Favourate_ThingsApp.swift
+//  Favourate Things
+//
+//  Created by Evelyn Chen on 2022-10-28.
+//
+
+import SwiftUI
+
+@main
+struct Favourate_ThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
